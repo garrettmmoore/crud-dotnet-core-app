7d83b6e7dfda837ca6e52b1ab56b49cdc4887dc2
Garrett Moore <garrettmarkmoore@gmail.com>
2020-04-06 01:51:04 -0700

    Add model for DonationDBContext

--- WebApi/Models/DonationDBContext.cs
+++ WebApi/Models/DonationDBContext.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApi.Models
 {
-    public class DonationDBContext
+    // Inherit DbContext class from Entity Framework
+    public class DonationDBContext:DbContext
     {
+        // Constructor to set up database connection settings
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) :base(options)
+        {
+            
+        }
+        
+        // property of DCandidate to create corresponding table
+        public DbSet<DCandidate> DCandidates { get; set; }
         
     }
 }
