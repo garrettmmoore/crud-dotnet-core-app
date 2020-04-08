dab4cf00da4a3c63d0ee095a1d5b24179e7a7016
Garrett Moore <garrettmarkmoore@gmail.com>
2020-04-08 02:12:22 -0700

    Removed unused styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import { store } from "./actions/store";
 import { Provider } from "react-redux";
 import DCandidates from "./components/DCandidates";
