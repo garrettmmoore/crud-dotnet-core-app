1a206e5034ae5febd39fa09d2c9719a63cb78fab
Garrett Moore <garrettmarkmoore@gmail.com>
2020-04-07 23:51:18 -0700

    Update root directory

---
+++ WebApi/WebApi/Migrations/20200407073854_InitialCreate2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "bloodGroub",
+                table: "DCandidates",
+                newName: "bloodGroup");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "bloodGroup",
+                table: "DCandidates",
+                newName: "bloodGroub");
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/DCandidate.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        public  string fullName { get; set; }
+        
+        [Column(TypeName = "nvarchar(16)")]
+        public string mobile { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        public string email { get; set; }
+        
+        public int age { get; set; }
+        
+        [Column(TypeName = "nvarchar(3)")]
+        public string bloodGroup { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        public string address { get; set; }
+    }
+}
