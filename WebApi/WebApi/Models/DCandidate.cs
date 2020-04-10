38bdda0117d894d608e6205c8d37c944bdd96953
Garrett Moore <garrettmarkmoore@gmail.com>
2020-04-10 15:04:21 -0700

    Fix "Data is Null" error
    Fixed the error by making the "age" field nullable.

--- WebApi/WebApi/Models/DCandidate.cs
+++ WebApi/WebApi/Models/DCandidate.cs
@@ -7,21 +7,21 @@ namespace WebApi.Models
     {
         [Key]
         public int id { get; set; }
-        
+
         [Column(TypeName = "nvarchar(100)")]
-        public  string fullName { get; set; }
-        
+        public string fullName { get; set; }
+
         [Column(TypeName = "nvarchar(16)")]
         public string mobile { get; set; }
-        
+
         [Column(TypeName = "nvarchar(100)")]
         public string email { get; set; }
-        
-        public int age { get; set; }
-        
+
+        public int? age { get; set; }
+
         [Column(TypeName = "nvarchar(3)")]
         public string bloodGroup { get; set; }
-        
+
         [Column(TypeName = "nvarchar(100)")]
         public string address { get; set; }
     }
